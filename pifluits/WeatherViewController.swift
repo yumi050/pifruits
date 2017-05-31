4f9b81cab3a578b16cdf4419159149389c1bca01
Dulth <yumi050@hotmail.com>
2017-05-31 18:38:00 +0900

    2回目の追加：5/31

---
+++ pifluits/WeatherViewController.swift
@@ -0,0 +1,51 @@
+//
+//  WeatherViewController.swift
+//  pifluits
+//
+//  Created by yumiH on 2017/05/27.
+//  Copyright © 2017年 yumiH. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+
+class WeatherViewController: UIViewController {
+  
+  
+  @IBOutlet var weatherLabel: UILabel!
+  
+  @IBOutlet var weatherImage: UIImageView!
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+      }
+  
+  
+  
+  
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
